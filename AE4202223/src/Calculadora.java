9888c4caa4ed0421ff78dd58a2bfc3ce1a3fd579
Miguel <alvarezmiguelrodriguez@gmail.com>
2023-03-28 18:33:02 +0200

    Version 2

--- AE4202223/src/Calculadora.java
+++ AE4202223/src/Calculadora.java
@@ -8,24 +8,56 @@ import java.util.Scanner;
 //--------------------------------------------------------------------------------------
 public class Calculadora 
 {
+
+	// Metodo principal
 	public static void main(String[] args) 
 	{
-		// Declaracion de objetos y variables
+		// Variabless
 		Scanner reader = new Scanner(System.in);
 		int numeroA;
 		int numeroB;
-		
-		// Solicitud de numeros por consola
+
+		// Solicitud de datos por consola
 		System.out.print("Introduce el primer numero entero: ");
 		numeroA = reader.nextInt();
 
 		System.out.print("Introduce el segundo numero entero: ");
 		numeroB = reader.nextInt();
 
-		// Impresión de resultados
-		System.out.println(numeroA + " + " + numeroB + " = " + (numeroA + numeroB));
-		System.out.println(numeroA + " - " + numeroB + " = " + (numeroA - numeroB));
-		System.out.println(numeroA + " * " + numeroB + " = " + (numeroA * numeroB));
-		System.out.println(numeroA + " / " + numeroB + " = " + (numeroA / numeroB));
+		// Salida de resutlados
+		ImpresionResultados(numeroA, numeroB);
+	}
+
+	// Metodo para la impresion de los resultados
+	private static void ImpresionResultados(int numeroA, int numeroB) 
+	{
+		System.out.println(numeroA + " + " + numeroB + " = " + Suma(numeroA, numeroB));
+		System.out.println(numeroA + " - " + numeroB + " = " + Resta(numeroA, numeroB));
+		System.out.println(numeroA + " * " + numeroB + " = " + Multiplicacion(numeroA, numeroB));
+		System.out.println(numeroA + " / " + numeroB + " = " + Division(numeroA, numeroB));
+	}
+
+	// Metodo para dividir dos numeros
+	private static float Division(int numeroA, int numeroB) 
+	{
+		return numeroA / (float) numeroB;
+	}
+	
+	// Metodo para  multiplicar dos numeros
+	private static int Multiplicacion(int numeroA, int numeroB) 
+	{
+		return numeroA * numeroB;
+	}
+	
+	// Metodo para restar dos numeros	
+	private static int Resta(int numeroA, int numeroB) 
+	{
+		return numeroA - numeroB;
+	}
+	
+	// Metodo para sumar dos numeros
+	private static int Suma(int numeroA, int numeroB) 
+	{
+		return numeroA + numeroB;
 	}
 }
